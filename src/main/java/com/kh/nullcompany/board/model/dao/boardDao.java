b5eff6758378d445afcc0d0004dc9a6039ff0747
kangd <kangd@LAPTOP-FMLTIPDU>
2020-11-04 23:03:45 +0900

    1104 동커밋

--- src/main/java/com/kh/nullcompany/board/model/dao/boardDao.java
+++ src/main/java/com/kh/nullcompany/board/model/dao/boardDao.java
@@ -9,48 +9,57 @@ import org.springframework.stereotype.Repository;
 
 import com.kh.nullcompany.board.model.vo.PageInfo;
 import com.kh.nullcompany.board.model.vo.board;
+import com.kh.nullcompany.board.model.vo.comment;
 
 @Repository("bDao")
 public class boardDao {
 
-	@Autowired
-	private SqlSessionTemplate sqlSession;
-	
-	public int getListCount() {
-		return sqlSession.selectOne("boardMapper.getListCount");
-	}
+   @Autowired
+   private SqlSessionTemplate sqlSession;
+   
+   public int getListCount() {
+      return sqlSession.selectOne("boardMapper.getListCount");
+   }
 
-	public ArrayList<board> selectListImport(PageInfo pi) {
-		int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
-		RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("boardMapper.selectListImport",null,rowBounds);
-	}
-	
-	public ArrayList<board> selectListAll(PageInfo pi) {
-		int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
-		RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("boardMapper.selectListAll",null,rowBounds);
-	}
+   public ArrayList<board> selectListImport(PageInfo pi) {
+      int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
+      RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
+      return (ArrayList)sqlSession.selectList("boardMapper.selectListImport",null,rowBounds);
+   }
+   
+   public ArrayList<board> selectListAll(PageInfo pi) {
+      int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
+      RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
+      return (ArrayList)sqlSession.selectList("boardMapper.selectListAll",null,rowBounds);
+   }
 
-	public ArrayList<board> selectListTeam(PageInfo pi) {
-		int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
-		RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("boardMapper.selectListTeam",null,rowBounds);
-	}
+   public ArrayList<board> selectListTeam(PageInfo pi) {
+      int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
+      RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
+      return (ArrayList)sqlSession.selectList("boardMapper.selectListTeam",null,rowBounds);
+   }
 
-	public ArrayList<board> selectListFree(PageInfo pi) {
-		int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
-		RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("boardMapper.selectListFree",null,rowBounds);
-	}
+   public ArrayList<board> selectListFree(PageInfo pi) {
+      int offset = (pi.getCurrentPage()-1)*pi.getBoardLimit();
+      RowBounds rowBounds = new RowBounds(offset,pi.getBoardLimit());
+      return (ArrayList)sqlSession.selectList("boardMapper.selectListFree",null,rowBounds);
+   }
 
-	public int updateCount(int bNo) {
-		return sqlSession.update("boardMapper.updateCount",bNo);
-	}
+   public int updateCount(int bNo) {
+      return sqlSession.update("boardMapper.updateCount",bNo);
+   }
 
-	public board selectBoard(int bNo) {
-		return sqlSession.selectOne("boardMapper.selectBoard",bNo);
-	}
+   public board selectBoard(int bNo) {
+      return sqlSession.selectOne("boardMapper.selectBoard",bNo);
+   }
+
+   public ArrayList<comment> selectCommentList(int bNo) {
+      return (ArrayList)sqlSession.selectList("boardMapper.selectCommentList",bNo);
+   }
+
+   public int insertComment(comment c) {
+      return sqlSession.insert("boardMapper.insertComment",c);
+   }
 
 
 }
--- src/main/java/com/kh/nullcompany/board/model/service/boardService.java
+++ src/main/java/com/kh/nullcompany/board/model/service/boardService.java
@@ -4,56 +4,61 @@ import java.util.ArrayList;
 
 import com.kh.nullcompany.board.model.vo.PageInfo;
 import com.kh.nullcompany.board.model.vo.board;
+import com.kh.nullcompany.board.model.vo.comment;
 
 public interface boardService {
 
-	
-	
-	
-	/**
-	 * 1_1. 게시판 총 갯수 조회
-	 * @return
-	 */
-	int getListCount();
+   
+   
+   
+   /**
+    * 1_1. 게시판 총 갯수 조회
+    * @return
+    */
+   int getListCount();
 
-	/**
-	 * 1_2. 중요게시판 리스트 조회
-	 * @param pi 
-	 * @return
-	 */
-	ArrayList<board> selectListImport(PageInfo pi);
+   /**
+    * 1_2. 중요게시판 리스트 조회
+    * @param pi 
+    * @return
+    */
+   ArrayList<board> selectListImport(PageInfo pi);
 
-	/**
-	 * 1_3. 사내공지 리스트 조회
-	 * @param pi
-	 * @return
-	 */
-	ArrayList<board> selectListAll(PageInfo pi);
+   /**
+    * 1_3. 사내공지 리스트 조회
+    * @param pi
+    * @return
+    */
+   ArrayList<board> selectListAll(PageInfo pi);
 
-	/**
-	 * 1_4. 팀내공지 리스트 조회
-	 * @param pi
-	 * @return
-	 */
-	ArrayList<board> selectListTeam(PageInfo pi);
+   /**
+    * 1_4. 팀내공지 리스트 조회
+    * @param pi
+    * @return
+    */
+   ArrayList<board> selectListTeam(PageInfo pi);
 
-	/**
-	 * 1_5. 자유게시판 리스트 조회
-	 * @param pi
-	 * @return
-	 */
-	ArrayList<board> selectListFree(PageInfo pi);
+   /**
+    * 1_5. 자유게시판 리스트 조회
+    * @param pi
+    * @return
+    */
+   ArrayList<board> selectListFree(PageInfo pi);
 
-	/**
-	 * 2. 게시판 상세조회
-	 * @param bId
-	 * @return
-	 */
-	board selectBoard(int bNo);
-	
-	
-	
-	
-	
+   /**
+    * 2. 게시판 상세조회
+    * @param bId
+    * @return
+    */
+   board selectBoard(int bNo);
+
+   ArrayList<comment> selectCommentList(int bNo);
+
+   int insertComment(comment c);
+   
+   
+   
+   
+   
 
 }
