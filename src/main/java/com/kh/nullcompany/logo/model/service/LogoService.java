11c26baf771d83f835c08a2ac4c84669d9c9f173
js01316 <js01316@hanmail.net>
2020-11-27 22:33:30 +0900

    20201127 10:33 ara 로고 등록

--- src/main/java/com/kh/nullcompany/logo/model/service/LogoService.java
+++ src/main/java/com/kh/nullcompany/logo/model/service/LogoService.java
@@ -1,5 +1,9 @@
 package com.kh.nullcompany.logo.model.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface LogoService {
+
+	int logoUpload(String logoFileName);
 
 }
